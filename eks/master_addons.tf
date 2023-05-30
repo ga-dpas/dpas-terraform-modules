e062ed9380547a5cd5ee14e263116e7a7a586e94
Nikita <nikita.r.gandhi@gmail.com>
2023-05-30 13:19:25 +1000

    Upgrade vpc module (#6)
    * upgrade vppc-module version to v4.0.2
    
    * adjust module configuration as per new version
    
    * eks addons - resolve_conflicts is deprecated

--- eks/master_addons.tf
+++ eks/master_addons.tf
@@ -4,10 +4,11 @@ resource "aws_eks_addon" "eks_addons" {
   cluster_name = aws_eks_cluster.eks.name
   addon_name   = try(each.value.name, each.key)
 
-  addon_version            = lookup(each.value, "addon_version", null)
-  resolve_conflicts        = lookup(each.value, "resolve_conflicts", null)
-  service_account_role_arn = lookup(each.value, "service_account_role_arn", null)
-  configuration_values     = lookup(each.value, "configuration_values", null)
+  addon_version               = lookup(each.value, "addon_version", null)
+  resolve_conflicts_on_create = lookup(each.value, "resolve_conflicts", null)
+  resolve_conflicts_on_update = lookup(each.value, "resolve_conflicts", null)
+  service_account_role_arn    = lookup(each.value, "service_account_role_arn", null)
+  configuration_values        = lookup(each.value, "configuration_values", null)
 
   depends_on = [
     aws_autoscaling_group.node,
--- eks/vpc.tf
+++ eks/vpc.tf
@@ -2,7 +2,7 @@
 ##############################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "v3.14.2"
+  version = "v4.0.2"
 
   count = var.create_vpc ? 1 : 0
 
@@ -32,7 +32,15 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  enable_nat_gateway = true
+  # One NAT Gateway per subnet (default behavior)
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = false
+
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+  map_public_ip_on_launch       = false
 
   create_database_subnet_group = var.create_database_subnet_group
   database_subnet_group_name   = var.database_subnet_group_name
