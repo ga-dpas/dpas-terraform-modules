01705dfeea84f12c9dd2cc46e18cc7e515ad39df
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2023-05-30 11:56:40 +1000

    adjust module configuration as per new version

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -32,7 +32,15 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  enable_nat_gateway = true
+  # One NAT Gateway per subnet (default behavior)
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = false
+
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+  map_public_ip_on_launch       = false
 
   create_database_subnet_group = var.create_database_subnet_group
   database_subnet_group_name   = var.database_subnet_group_name
