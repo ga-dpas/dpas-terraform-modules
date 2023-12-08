876c9d1a6844a01a2bb364c436b73a779f773558
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2023-12-08 13:43:21 +1100

    simplified default worker node group user-data template

--- eks/worker_userdata.tpl.sh
+++ eks/worker_userdata.tpl.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 set -o xtrace
-# Get instance and ami id from the aws ec2 metadate endpoint
-%{if enable_imdsv2~}
-token=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 3600"`
-id=`curl -H "X-aws-ec2-metadata-token: $token" -v http://169.254.169.254/latest/meta-data/instance-id`
-ami=`curl -H "X-aws-ec2-metadata-token: $token" -v http://169.254.169.254/latest/meta-data/ami-id`
-%{else~}
-id=$(curl http://169.254.169.254/latest/meta-data/instance-id -s)
-ami=$(curl http://169.254.169.254/latest/meta-data/ami-id -s)
-%{endif~}
-/etc/eks/bootstrap.sh --apiserver-endpoint '${endpoint}' --b64-cluster-ca '${certificate_authority}' '${cluster_id}' ${extra_bootstrap_args} \
---kubelet-extra-args \
-  "--node-labels=cluster=${cluster_id},instance-id=$id,ami-id=$ami,${extra_node_labels} \
-   --cloud-provider=aws ${extra_kubelet_args}"
+/etc/eks/bootstrap.sh ${cluster_id} ${extra_bootstrap_args} --apiserver-endpoint ${endpoint} --b64-cluster-ca ${certificate_authority}
 ${extra_userdata}
