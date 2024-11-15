d094c253f09345a58564889f4fc88b49dfa2cf6a
Nikita Gandhi <nikitagandhi@Nikitas-MacBook-Pro-3.local>
2024-11-15 15:11:24 +1100

    update eks cluster role settings

--- eks/master_policy.tf
+++ eks/master_policy.tf
@@ -1,44 +1,29 @@
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    sid     = "EKSClusterAssumeRole"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "eks_cluster" {
   name = "${var.cluster_id}-cluster-role"
 
-  assume_role_policy = <<-POLICY
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+
+  tags = merge(
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "eks.${local.dns_suffix}"
-          },
-          "Action": "sts:AssumeRole"
-        }
-      ]
-    }
-  POLICY
-
-  # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/920
-  # Resources running on the cluster are still generating logs when destroying the module resources
-  # which results in the log group being re-created even after Terraform destroys it. Removing the
-  # ability for the cluster role to create the log group prevents this log group from being re-created
-  # outside of Terraform due to services still generating logs during destroy process
-  dynamic "inline_policy" {
-    for_each = var.create_cluster_cloudwatch_log_group ? [1] : []
-    content {
-      name = "${var.cluster_id}-cluster-policy"
-
-      policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-          {
-            Action   = ["logs:CreateLogGroup"]
-            Effect   = "Deny"
-            Resource = "*"
-          },
-        ]
-      })
-    }
-  }
-
+      Name        = "${var.cluster_id}-cluster-role"
+      owner       = var.owner
+      namespace   = var.namespace
+      environment = var.environment
+    },
+    var.tags
+  )
 }
 
 resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
@@ -50,4 +35,3 @@ resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks_cluster.name
 }
-
